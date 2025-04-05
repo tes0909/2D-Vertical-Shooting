a02f73328d27b9f79ffcae9b2f68510248def7be
tes0909 <krosa1213@gmail.com>
2025-04-05 15:32:42 +0900

    feat: 스타트씬 전투기 반복이동

---
+++ Assets/Scripts/JetFly.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetFly : MonoBehaviour
+{
+    [SerializeField] private float startY = -3.5f;
+    [SerializeField] private float endY = 6f;
+    [SerializeField] private float speed = 10f;
+    void Start()
+    {
+        ResetPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void ResetPosition()
+    {
+        Vector3 pos = transform.position;
+        pos.y = startY;
+        transform.position = pos;
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.up * (speed * Time.deltaTime));
+
+        if (transform.position.y > endY)
+        {
+            ResetPosition();
+        }
+    }
+}
