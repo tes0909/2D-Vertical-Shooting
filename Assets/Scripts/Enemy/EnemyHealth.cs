d68002478571d1449d032ebb15edcd564614ba70
tes0909 <krosa1213@gmail.com>
2025-04-19 21:37:06 +0900

    feat: 적 hp 표시

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -2,19 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] private int health;
+    [SerializeField] private int currentHealth;
+    [SerializeField] private int maxHealth;
     [SerializeField] private int enemyscore;
     [SerializeField] private Sprite[] sprites;
     [SerializeField] private float returnSpriteDelay = 0.1f; // 스프라이트 변경 후 지연 시간
+    [SerializeField] private Slider healthBar;
     
     private int _damagedSpriteIndex = 1; // 피해 입었을 때 사용할 스프라이트 인덱스
     private int _defaultSpriteIndex; // 기본 인덱스
     private SpriteRenderer _spriteRenderer;
     private EnemyItemDrop _enemyItemDrop;
-    private int _currentHealth;
     
     void Awake()
     {
@@ -24,20 +27,31 @@ public class EnemyHealth : MonoBehaviour
 
     private void OnEnable()
     {
-        _currentHealth = health;
         _spriteRenderer.sprite = sprites[_defaultSpriteIndex];
         transform.rotation = Quaternion.identity;
     }
 
+    private void Start()
+    {
+        if (healthBar != null)
+            healthBar.value = (float)currentHealth / maxHealth;
+    }
+
+    private void Update()
+    {
+        if (healthBar != null)
+            healthBar.value = (float)currentHealth / maxHealth;
+    }
+
     public void TakeDamaged(int damage)
     {
-        if(_currentHealth <= 0) return;
-        _currentHealth -= damage;
+        if(currentHealth <= 0) return;
+        currentHealth -= damage;
 
         _spriteRenderer.sprite = sprites[_damagedSpriteIndex];
         StartCoroutine(ReturnSprite());
 
-        if (_currentHealth <= 0)
+        if (currentHealth <= 0)
         {
             GameManager.Instance.AddScore(enemyscore);
             _enemyItemDrop.ItemDrop();
